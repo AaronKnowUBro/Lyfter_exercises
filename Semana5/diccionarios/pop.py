e933dbb39b4df93bc90561de9ffa3df3ae6d23e8
Aaron David Calderon Alfaro <acalderonalfaro5@gmail.com>
2026-05-27 19:21:42 -0600

    added past exercises

--- Semana5/diccionarios/pop.py
+++
@@ -1,7 +0,0 @@
-list_of_keys = ['access_level', 'age']
-employee = {'name': 'Aaron', 'email': 'aaron@aaron.com', 'access_level': 5, 'age': 25}
-
-for key in list_of_keys:
-    employee.pop(key, None)
-
-print(employee)
--- semana6/splitted_text.py
+++
@@ -1,4 +0,0 @@
-def sort_words(text):
-    return '-'.join(sorted(text.split('-')))
-
-print(sort_words("CaseOh-vegetta777-Dashiegames-Alexby-Mangel"))
